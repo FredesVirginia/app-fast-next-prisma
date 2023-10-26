7fac7686a41c23893faeebfbb8a62e705731df6f
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2023-10-26 21:02:40 +0000

    por la navegacion

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-blue-300 ${inter.className}`} >{
-      children}
+      <body className={`bg-blue-300 ${inter.className}`} >
+        <NavBar/>
+      {children}
       </body>
     </html>
   )
